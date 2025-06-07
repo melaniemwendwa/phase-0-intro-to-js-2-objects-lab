a13f6c304ad3c717b33aea8e92d6149ebfba92d9
melanie mwendwa <mwendwathuranira@gmail.com>
2025-06-07 16:42:12 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const employee = {
+    name:"Melanie",
+    streetAddress:"Nairobi",
+}
+
+
+//non destructively update employee
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value,
+  };
+}
+
+//destructively update employee
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value; 
+
+  return obj;
+}
+
+//Non-destructively delete
+function deleteFromEmployeeByKey(obj, key, value){
+    const newEmployee = {...obj};
+    delete newEmployee[key];
+    return newEmployee
+}
+
+//Destructively delete
+function destructivelyDeleteFromEmployeeByKey(Obj, key) {
+  delete Obj[key]; 
+  return employee;
+}
